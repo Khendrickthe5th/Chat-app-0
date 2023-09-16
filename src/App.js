56784d195b14d1b773fd1ec9b4f63b86fb1d2c6c
Ozoani Chidubem <flyinglego153971@gmail.com>
2023-09-16 09:13:45 +0100

    few lines of words for testing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>Sup world</h1>
+      <p>In conclusion, the MERN stack project structure is organized in a way that separates the backend and frontend code. It also follows the MVC architecture pattern. The client directory contains the frontend code, and the server directory contains the backend code. The MERN stack project structure helps in building scalable and robust web applications. By separating the concerns of the application, the codebase becomes more manageable and easier to maintain. If you're looking to build a web application using the MERN stack, it's highly recommended that you follow this project structure.</p>
     </div>
   );
 }
